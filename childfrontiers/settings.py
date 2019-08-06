e6a58f9c7c284f6d4819b0d6a83f6fca6ca62ac8
Rajendra Aldis <rajendra.aldis@gmail.com>
2019-08-06 15:12:52 -0400

    Added Heroku deployment files

--- childfrontiers/settings.py
+++ childfrontiers/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3&ex_!(gei+-6p3xpn82@gr_()k6c@bf95x3$%vz1!jj1smjvh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3&ex_!(gei+-6p3xpn82@gr_()k6c@bf95x3$%vz1!jj1smjvh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,8 +117,12 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
@@ -125,3 +130,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Sends emails to console. Email backend not yet configured.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Security settings for deployment per https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
